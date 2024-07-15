e18a46e6331b4cc529e27e321ec346825da6e7f1
Ignacio Quirós <ignacio.quiros.sordo@gmail.com>
2024-07-15 16:35:06 +0000

    [Fixed Footer and background img opacity]

--- src/js/component/Footer.js
+++ src/js/component/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='container-fluid bg-primary bg-gradient border-top border-5 border-dark py-3 mt-5'>
+        <div className='container-fluid bg-primary bg-gradient border-top border-5 border-dark py-3 mt-5' style={{color: "white", textAlign: "center", padding: "10px", position: "absolute", bottom: "0", width: "100%"}}>
             <div>Made by <strong>Ignacio Quirós</strong></div>
         </div>
     )
